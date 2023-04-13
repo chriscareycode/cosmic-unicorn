2528e2a9817ec015e816db67cf3842a791793384
Chris Carey <chris@chriscarey.com>
2023-04-13 08:00:33 -0700

    add script to delete files we absolutely do not need in the build (to reduce size)

---
+++ delete-extra-build-files.sh
@@ -0,0 +1,9 @@
+rm pico/emoji_paint/asset-manifest.json
+rm pico/emoji_paint/favicon.ico
+rm pico/emoji_paint/logo*.png
+rm pico/emoji_paint/manifest.json
+rm pico/emoji_paint/robots.txt
+
+rm pico/emoji_paint/static/css/*.map
+rm pico/emoji_paint/static/js/*.map
+rm pico/emoji_paint/static/js/*.txt
