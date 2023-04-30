66dd8099f7332551dfb3f02dd5dda2b0f63c2b06
Chris Carey <chris@chriscarey.com>
2023-04-29 20:47:03 -0700

    Update READMEs and some more files for cosmic-paste

--- cosmic-paste/README.md
+++ cosmic-paste/README.md
@@ -2,4 +2,45 @@
 
 A vanilla JavaScript/HTML page to paste from the clipboard to the Cosmic Unicorn
 
+## Install required libraries
+
+Connect your Pico to the computer and run Thonny. Select your Pico by clicking on the bottom right corner in Thonny. You should see the list of files on the Pico in the bottom left section. Stop any running process by clicking the Stop icon.
+
+In Thonny, go to Tools -> Manage Packages...
+
+Search for `microdot_asyncio` and install that. This will copy some files into the lib/ folder on your pico. It is needed for the web server that we use (same as cosmic_paint).
+
+## Setup WIFI_CONFIG.py
+
+This script uses and requires the same `WIFI_CONFIG.py` that many of the other WiFi examples use (like cosmic_paint). That will need to be setup like this:
+```
+SSID="fill this in with network name"
+PSK="fill this in with password"
+COUNTRY="GB or US or your country"
+```
+
+## Installing the server code without user interface
+
+- From the repo, upload `emoji_paint.py` on your Pico.
+- In Thonny, run the file on the Pico, and get the IP address from the console
+- If it does not run, or you do not see the IP address, then setup `WIFI_CONFIG.py` or install required libraries and try again.
+- Optionally copy `emoji_paint.py` to `main.py` if you want it to start on boot
+
+- Copy `cosmic-paste/config-unicorns.example.json` to `cosmic-paste/config-unicorns.json`
+- Edit the file `cosmic-paste/config-unicorns.json` with your Unicorn IP address.
+- Upload `cosmic-paste/config-unicorns.json` to your Pico.
+- Upload `paint.html` on your Pico
+
+Load the user interface at http://<your-pico-ip>/paint.html
+
+## Local Development
+
+- `python -m SimpleHTTPServer 3070`
+
+or
+
+- `python -m http.server 3070`
+
+Open http://localhost:3070/paste.html in the browser
+
 [Back to top](https://github.com/chriscareycode/cosmic-unicorn/)
---
+++ cosmic-paste/emoji_paint.py
@@ -0,0 +1,104 @@
+import os
+from microdot_asyncio import Microdot, Request, Response, send_file
+from phew import connect_to_wifi
+from cosmic import CosmicUnicorn
+from picographics import PicoGraphics, DISPLAY_COSMIC_UNICORN as DISPLAY
+from WIFI_CONFIG import SSID, PSK
+
+cu = CosmicUnicorn()
+graphics = PicoGraphics(DISPLAY)
+mv_graphics = memoryview(graphics)
+cu.set_brightness(0.5)
+
+WIDTH, HEIGHT = graphics.get_bounds()
+
+ip = connect_to_wifi(SSID, PSK)
+
+print(f"Start painting at: http://{ip}")
+
+last_pixels = ""
+
+server = Microdot()
+
+@server.route("/", methods=["GET"])
+def route_index(request):
+    return send_file("emoji_paint/index.html")
+
+@server.route("/config-unicorns.json", methods=["GET"])
+def route_index(request):
+    return send_file("emoji_paint/config-unicorns.json")
+
+@server.route("/static/<path:path>", methods=["GET"])
+def route_static(request, path):
+    return send_file(f"emoji_paint/static/{path}")
+
+@server.route('/get_pixels', methods=["GET"])
+def route_get_pixels(req):
+    global last_pixels
+    #print("get_pixels")
+        
+    res = Response()
+    res.headers["Access-Control-Allow-Origin"] = '*'
+    res.body = last_pixels
+    return res
+    
+
+@server.post('/set_pixels')
+def set_pixels(req):
+    global last_pixels
+    #print("got data")
+
+    data = req.body
+    
+    # save for later
+    last_pixels = data
+        
+    arr = list(data)
+    
+    for j in range(HEIGHT):
+        for i in range(WIDTH):
+            index = (j * 32 + i) * 4
+                        
+            #convert rgba to rgb
+            r = int(data[index])
+            g = int(data[index+1])
+            b = int(data[index+2])
+            a = int(data[index+3]) / 255
+            
+            r = round(a * r)
+            g = round(a * g)
+            b = round(a * b)
+            
+            # set the pixel
+            graphics.set_pen(graphics.create_pen(r, g, b))
+            graphics.pixel(i, j)
+    cu.update(graphics)
+                
+    res = Response()
+    res.headers["Access-Control-Allow-Origin"] = '*'
+    res.body = "success"
+    return res
+
+@server.route("/get_brightness", methods=["GET"])
+def get_brightness(req):
+    res = Response()
+    res.headers["Access-Control-Allow-Origin"] = '*'
+    res.body = str(cu.get_brightness())
+    return res
+
+@server.post("/set_brightness")
+def set_brightness(req):
+    res = Response()
+    res.headers["Access-Control-Allow-Origin"] = '*'
+    res.body = "failure"
+    
+    brightness = req.body
+    if brightness:
+        brightnessFloat = float(brightness)
+        if brightnessFloat >= 0 and brightnessFloat <= 10:
+            cu.set_brightness(brightnessFloat)
+            res.body = "success"
+    return res
+
+# start the web server on all ips, port 80
+server.run(host="0.0.0.0", port=80)
