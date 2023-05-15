5f34c2b5833b05bf7cef7b4d97a0048eff10965d
Chris Carey <chris@chriscarey.com>
2023-05-15 09:10:15 -0700

    Redraw the pixels after brightness changes

--- cosmic-emoji-react/pico/emoji_paint.py
+++ cosmic-emoji-react/pico/emoji_paint.py
@@ -14,11 +14,14 @@ WIDTH, HEIGHT = graphics.get_bounds()
 
 ip = connect_to_wifi(SSID, PSK)
 
-print(f"Start painting at: http://{ip}")
+print(f"Connected to wifi [{SSID}] ip [{ip}]")
 
 last_pixels = ""
 
+# start web server
 server = Microdot()
+
+print(f"Start painting at: http://{ip}")
 
 @server.route("/", methods=["GET"])
 def route_index(request):
@@ -41,18 +44,10 @@ def route_get_pixels(req):
     res.headers["Access-Control-Allow-Origin"] = '*'
     res.body = last_pixels
     return res
-    
 
-@server.post('/set_pixels')
-def set_pixels(req):
+def draw_pixels():
     global last_pixels
-    #print("got data")
-
-    data = req.body
-    
-    # save for later
-    last_pixels = data
-        
+    data = last_pixels
     arr = list(data)
     
     for j in range(HEIGHT):
@@ -73,6 +68,15 @@ def set_pixels(req):
             graphics.set_pen(graphics.create_pen(r, g, b))
             graphics.pixel(i, j)
     cu.update(graphics)
+
+@server.post('/set_pixels')
+def set_pixels(req):
+    global last_pixels
+
+    # save for later
+    last_pixels = req.body
+
+    draw_pixels()
                 
     res = Response()
     res.headers["Access-Control-Allow-Origin"] = '*'
@@ -98,6 +102,9 @@ def set_brightness(req):
         if brightnessFloat >= 0 and brightnessFloat <= 10:
             cu.set_brightness(brightnessFloat)
             res.body = "success"
+            # after changing brightness you have to draw the pixels again
+            # to see the brightness change
+            draw_pixels()
     return res
 
 # start the web server on all ips, port 80
